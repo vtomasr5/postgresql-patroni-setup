aad0ffc2a0a3b397b51f0dc6b395299d15805f43
Vicenç Juan Tomàs Montserrat <vtomasr5@gmail.com>
2018-12-06 19:08:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+postgresql-patroni-setup
+========================
+
+# Overview
+
+This repo it helps you quickly spin up a 2-node cluster of PostgreSQL, managed by Patroni.
+
+It's intended as a playground for us, and a learning resource that we wanted to share with the community.
+
+# What's in the cluster?
+
+When you start the cluster, you get 2 nodes, each running:
+
+  - PostgreSQL
+  - Patroni
+  - HAProxy
+
+All packages are from Ubuntu 16.04, except for PostgreSQL itself, which is at version 9.6.
+
+The cluster is configured with a single primary and one asynchronous replica.
+
+# Dependencies
+1. [Virtualbox](https://www.virtualbox.org/wiki/Downloads)
+2. [Vagrant](http://www.vagrantup.com/downloads.html)
+3. `git clone https://github.com/vtomasr5/postgresql-patroni-setup.git`
+
+# Getting started
+
+1.  On 2 separate windows:
+2.  `vagrant up pg01 && vagrant ssh pg01`
+3.  wait 20 seconds before starting pg02
+4.  `vagrant up pg02 && vagrant ssh pg02`
+
+# Viewing cluster status
+
+Get patroni information from his members
+  - patronictl list
+
+# Connecting to PostgreSQL
+
+You can connect via HAproxy using the balancing IP: 172.28.33.11:5000
+
+# TODO
+
+- [ ] Add pgBouncer support
+- [ ] Test on a Windows host (tested on Linux and macOS hosts)
+- [ ] Add repmgr support
+- [ ] Improve documentation
+
+# I have a question!
+
+We're happy to receive questions as issues on this repo, so don't be shy!
+
+It's hard to know exactly what documentation/guidance is useful to people, so we'll use the questions we answer to improve this README and link out to more places you can read up on the technologies we're using.
+
+# Further reading
+
+* [PostgreSQL and Patroni cluster](https://www.linode.com/docs/databases/postgresql/create-a-highly-available-postgresql-cluster-using-patroni-and-haproxy/#before-you-begin)
+
+# References
+* [PostgreSQL](https://www.postgresql.org)
+* [Patroni](https://patroni.readthedocs.io/en/latest/)
+* [HAProxy](https://www.haproxy.org/)
+* [Vagrant](http://vagrantup.com)
+* [VirtualBox](http://www.virtualbox.org)
---
+++ postgresql-cluster-setup.sh
@@ -0,0 +1,251 @@
+#!/bin/bash
+
+PG01="172.28.33.11"
+PG02="172.28.33.12"
+
+POSTGRESQL_VERSION=9.6
+PGBOUNCER_VERSION=1.9.0-2.pgdg16.04+1
+CONSUL_VERSION=1.4.0
+PATRONI_VERSION=1.5.3
+
+function setup_ssh_keys() {
+    cp -rp /vagrant/.ssh/* /root/.ssh
+    cp -rp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+    chmod 700 /root/.ssh
+    chmod 600 /root/.ssh/id_rsa
+    chmod 644 /root/.ssh/id_rsa.pub
+    chown -R root /root/.ssh
+    chgrp -R root /root/.ssh
+}
+
+function setup_python() {
+    apt-get -y install python python-pip wget unzip
+}
+
+function setup_patroni() {
+    # Install
+    pip install patroni[consul]==${PATRONI_VERSION}
+    pip install psycopg2-binary
+    mkdir -p /etc/patroni
+
+    # Configure
+    cp -p /vagrant/patroni.yml /etc/patroni/patroni.yml
+    cp -p /vagrant/patroni.service /etc/systemd/system/ 
+    systemctl daemon-reload
+
+    systemctl start patroni
+}
+
+function setup_consul() {
+    # Install
+    wget -c https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+    unzip consul_${CONSUL_VERSION}_linux_amd64.zip
+    mv consul /usr/local/bin/
+    rm -f consul_${CONSUL_VERSION}_linux_amd64.zip
+    cp -rp /vagrant/consul.d /etc/
+    mkdir -p /var/consul/ui
+
+    # Configure consul
+    if [ "$(hostname)" == "pg01" ]; then # consul server
+        mkdir -p /var/consul/server
+        cp -p /vagrant/consul-server.service /etc/systemd/system/
+    elif [ "$(hostname)" == "pg02" ]; then # consul client (agent)
+        mkdir -p /var/consul/client
+        cp -p /vagrant/consul-client.service /etc/systemd/system/
+    fi
+    systemctl daemon-reload
+
+    if [ "$(hostname)" == "pg01" ]; then # consul server
+        systemctl start consul-server
+    elif [ "$(hostname)" == "pg02" ]; then # consul client (agent)
+        systemctl start consul-client
+    fi
+}
+
+function setup_haproxy() {
+    # Install
+    apt-get install haproxy -y
+
+    # Configure
+    cp -p /vagrant/haproxy.cfg /etc/haproxy/haproxy.cfg
+
+    systemctl restart haproxy
+    # Check syntax if fails: /usr/sbin/haproxy -c -V -f /etc/haproxy/haproxy.cfg
+}
+
+function setup_pgbouncer() {
+    # Install the version that comes with the official apt postgresql repository
+    apt-get -y install pgbouncer=${PGBOUNCER_VERSION}
+
+    cat > /etc/pgbouncer/pgbouncer.ini <<EOF
+[databases]
+postgres = host=172.28.33.10 pool_size=6
+template1 = host=172.28.33.10 pool_size=6
+
+[pgbouncer]
+logfile = /var/log/postgresql/pgbouncer.log
+pidfile = /var/run/postgresql/pgbouncer.pid
+listen_addr = *
+listen_port = 6432
+unix_socket_dir = /var/run/postgresql
+auth_type = trust
+auth_file = /etc/pgbouncer/userlist.txt
+admin_users = postgres
+stats_users =
+pool_mode = transaction
+server_reset_query =
+server_check_query = select 1
+server_check_delay = 10
+max_client_conn = 1000
+default_pool_size = 12
+reserve_pool_size = 5
+log_connections = 1
+log_disconnections = 1
+log_pooler_errors = 1
+EOF
+
+    cat > /etc/pgbouncer/userlist.txt <<EOF
+"postgres" "whatever_we_trust"
+EOF
+
+    cat > /etc/default/pgbouncer <<EOF
+START=1
+EOF
+    systemctl restart pgbouncer
+}
+
+function setup_postgresql_repo() {
+    # Setup postgresql repo
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+
+    # Setup postgresql repo key
+    apt-get -y install wget ca-certificates ntp
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+    # Update package info
+    apt-get update
+    # Upgrade all the (safe) packages to start from a clean slate
+    apt-get -y upgrade
+}
+
+
+function setup_postgresql() {
+    # Install postgresql
+    apt-get -y install postgresql-${POSTGRESQL_VERSION}
+
+    systemctl stop postgresql
+
+    cat > /etc/postgresql/${POSTGRESQL_VERSION}/main/pg_hba.conf <<EOF
+local   all             postgres                                peer
+
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     peer
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            md5
+# IPv6 local connections:
+host    all             all             ::1/128                 md5
+# Allow replication connections from localhost, by a user with the
+# replication privilege.
+#local   replication     postgres                                peer
+#host    replication     postgres        127.0.0.1/32            md5
+host    replication     postgres        ::1/128                 md5
+hostssl    replication     postgres 172.28.33.11/32                 trust
+hostssl    replication     postgres 172.28.33.12/32                 trust
+# for user connections
+host       all     postgres 172.28.33.1/32                 trust
+hostssl    all     postgres 172.28.33.1/32                 trust
+# for pgbouncer
+host       all     postgres 172.28.33.10/32                 trust
+hostssl    all     postgres 172.28.33.10/32                 trust
+host       all     postgres 172.28.33.11/32                 trust
+hostssl    all     postgres 172.28.33.11/32                 trust
+host       all     postgres 172.28.33.12/32                 trust
+hostssl    all     postgres 172.28.33.12/32                 trust
+EOF
+
+    cat > /etc/postgresql/${POSTGRESQL_VERSION}/main/postgresql.conf <<EOF
+archive_command = 'exit 0'
+archive_mode = 'on'
+autovacuum = 'on'
+checkpoint_completion_target = 0.6
+#checkpoint_segments = 10
+checkpoint_warning = 300
+data_directory = '/var/lib/postgresql/${POSTGRESQL_VERSION}/main'
+datestyle = 'iso, mdy'
+default_text_search_config = 'pg_catalog.english'
+effective_cache_size = '128MB'
+external_pid_file = '/var/run/postgresql/${POSTGRESQL_VERSION}-main.pid'
+hba_file = '/etc/postgresql/${POSTGRESQL_VERSION}/main/pg_hba.conf'
+hot_standby = 'on'
+ident_file = '/etc/postgresql/${POSTGRESQL_VERSION}/main/pg_ident.conf'
+include_if_exists = 'repmgr_lib.conf'
+lc_messages = 'C'
+listen_addresses = '*'
+log_autovacuum_min_duration = 0
+log_checkpoints = 'on'
+logging_collector = 'on'
+log_min_messages = DEBUG3
+log_filename = 'postgresql.log'
+log_connections = 'on'
+log_directory = '/var/log/postgresql'
+log_disconnections = 'on'
+log_line_prefix = '%t [%p]: [%l-1] user=%u,db=%d,app=%a '
+log_lock_waits = 'on'
+log_min_duration_statement = 0
+log_temp_files = 0
+maintenance_work_mem = '128MB'
+max_connections = 100
+max_wal_senders = 5
+port = 5432
+shared_buffers = '128MB'
+shared_preload_libraries = 'pg_stat_statements'
+ssl = on
+ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'
+unix_socket_directories = '/var/run/postgresql'
+wal_buffers = '8MB'
+wal_keep_segments = '200'
+wal_level = 'hot_standby'
+work_mem = '128MB'
+EOF
+}
+
+#if [ ! -f /root/.ssh/id_rsa ]; then
+#    setup_ssh_keys
+#fi
+
+# Refresh repository packages
+apt-get update
+
+if [ ! -f /usr/bin/pip ]; then
+    setup_python
+fi
+
+if [ ! -f /usr/local/bin/patroni ]; then
+    setup_patroni
+fi
+
+if [ ! -f /usr/local/bin/consul ]; then
+    setup_consul
+fi
+
+# Install HAProxy only in the "master" when bootstrapping
+if [ "$(hostname)" == "pg01" ]; then
+    if [ ! -f /etc/haproxy/haproxy.cfg ]; then
+        setup_haproxy
+    fi
+fi
+
+if [ ! -f /etc/apt/sources.list.d/pgdg.list ]; then
+    setup_postgresql_repo
+fi
+
+if [ ! -f /etc/postgresql/${POSTGRESQL_VERSION}/main/postgresql.conf ]; then
+    setup_postgresql
+fi
+
+#if [ ! -f /etc/pgbouncer/pgbouncer.ini ]; then
+#    setup_pgbouncer
+#fi
